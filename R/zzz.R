c5f494b984323868615edbd3eae9428a9fcb9d57
Petrenko Kate <petrenko.kate@icloud.com>
2026-04-17 12:45:29 +0300

    build: move ggplot2/ComplexHeatmap/reticulate to Imports

--- R/zzz.R
+++ R/zzz.R
@@ -3,10 +3,15 @@
 #' @importFrom utils packageVersion
 #' @importFrom basilisk BasiliskEnvironment basiliskStart basiliskStop
 #'   basiliskRun
+#' @importFrom reticulate import py_to_r r_to_py
 #' @importFrom S4Vectors DataFrame
 #' @importFrom SummarizedExperiment SummarizedExperiment assay assayNames
 #'   colData
 #' @importFrom SingleCellExperiment SingleCellExperiment
+#' @importFrom ggplot2 ggplot aes geom_boxplot geom_bar geom_segment geom_point
+#'   geom_text geom_hline labs theme_minimal theme element_text element_blank
+#'   coord_equal scale_fill_manual scale_color_manual
+#' @importFrom grid unit
 #' @importClassesFrom S4Vectors DataFrame
 #' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #' @importClassesFrom SingleCellExperiment SingleCellExperiment
