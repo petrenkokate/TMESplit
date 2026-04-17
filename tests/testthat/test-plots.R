e9015d3f78c828424e210805ff9b94742484e370
Petrenko Kate <petrenko.kate@icloud.com>
2026-04-17 15:28:45 +0300

    test: suppress Rplots.pdf artifact from plot tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -3,6 +3,8 @@
 test_that("plotPrograms returns a Heatmap and draws", {
     skip_if_not_installed("ComplexHeatmap")
     res <- make_mock_result()
+    grDevices::pdf(nullfile())
+    on.exit(grDevices::dev.off(), add = TRUE)
     ht <- plotPrograms(res)
     expect_s4_class(ht, "Heatmap")
 })
@@ -10,6 +12,8 @@ test_that("plotPrograms returns a Heatmap and draws", {
 test_that("plotPrograms works with no specific programs", {
     skip_if_not_installed("ComplexHeatmap")
     res <- make_mock_result(k_spec_A = 0L, k_spec_B = 0L)
+    grDevices::pdf(nullfile())
+    on.exit(grDevices::dev.off(), add = TRUE)
     ht <- plotPrograms(res)
     expect_s4_class(ht, "Heatmap")
 })
